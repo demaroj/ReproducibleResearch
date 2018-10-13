bc9bf9e449836ba2b8cd4e0a56edf0fd0f9e6876
Joe DeMaro <demaroj@gmail.com>
2018-10-13 12:19:23 -0500

    Updates to processing.

--- FinalProject/WeatherServiceReport.R
+++ FinalProject/WeatherServiceReport.R
@@ -1,48 +1,56 @@
-## Loading and preprocessing the data
+#Analysis of NOAA Data to Visualize Differences in Population Health and Economics by Weather Event Types 
+ 
+###Synopsis: Raw data collected by NOAA provides event based data for every storm event in the United States for over 50 years.  
 
-#library(ggplot2)
+
+###Loading and preprocessing the data
+library(ggplot2)
 library(dplyr)
 library(knitr)
-#library(mice)
 
-#set working dir
-setwd("c:\\rprograms\\reproducibleresearch\\reproducibleresearch")
+###set working dir
+setwd("C:\\RPrograms\\ReproducibleResearch\\ReproducibleResearch\\FinalProject\\ReproducibleResearch\\FinalProject")
 
-#download and unzip raw data from website
-zipfile <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
-download.file(zipfile, tf <- tempfile(fileext = ".bz2"))
-#unzip(tf, exdir = td <- file.path(tempdir(), "myzip"))
+###download and unzip raw data from website
+datafile <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
+download.file(datafile, tf <- tempfile(fileext = ".bz2"))
 
-# read csv data create dataframe
+###read csv data and create rawdata dataframe
 rawdata <- read.csv(tf)
-tidydata <- rawdata[, c(1,2,7,23,24,25,26,27,28)]
+###Create tidydata as a subset of the rawdata
+tidydata <- rawdata[, c(1,2,7,8, 23,24,25,26,27,28)]
 
+###add column to sum fatalities and injury number 
 tidydata$death_injury <- as.numeric(as.character(tidydata$FATALITIES))+as.numeric(as.character(tidydata$INJURIES))
-#tidydata$economic <- as.numeric(as.character(tidydata$PROPDMG))*tidydata$PROPDMGEXP + as.numeric(as.character(tidydata$CROPDMG))*tidydata$CROPDMGEXP
 
-# fun <- function(x){dplyr::case_when(tidydata$PROPDMGEXP == "M" ~ 1e6,
-#                                     tidydata$PROPDMGEXP == "B" ~ 1e9,
-#                                     tidydata$PROPDMGEXP == "K" ~ 1e3,
-#                                     TRUE ~ 1)}
-# expRows <- length(tidydata$PROPDMGEXP)
-# tidydata$PROPDMGEXP_convert <- (sapply(1:expRows,FUN = fun))*1
-
-# for(i in 1:length(tidydata$PROPDMGEXP)) {
-#   if(tidydata[i,]$PROPDMGEXP=='M') {tidydata[i,]$PROPDMG_convert <- tidydata[i,]$PROPDMG*1e6}
-#   if(tidydata[i,]$PROPDMGEXP=='K') {tidydata[i,]$PROPDMG_convert <- tidydata[i,]$PROPDMG*1e3}
-# }
-
+###create a vector to hold and allow for conversion of exp coding
 exponent_Finance <- tidydata$PROPDMGEXP 
-exp_value <- sapply(exponent_Finance, function(x)if (x=='K') {1000} else if (x=='M') {1000000} else if (x=='B') {1000000000} else {0})
+exp_value <- sapply(exponent_Finance, function(x)  if (x=='K') {1000} else if (x=='M') {1000000} else if (x=='H') {100} else if (x=='B') {1000000000} else {0})
+###create a vector to hold and allow for conversion of exp coding
+exponent_Crop <- tidydata$CROPDMGEXP 
+exp_crop <- sapply(exponent_Crop, function(x) if (x=='K') {1000} else if (x=='M') {1000000} else if (x=='H') {100} else if (x=='B') {1000000000} else {0})
+###bind new columns to tidydata set containing number value for alpha label
+tidydata <- cbind(tidydata,exp_value, exp_crop)
 
+###add column that calculates property and crop damage in $                     
+tidydata$economic <- (as.numeric(as.character(tidydata$PROPDMG))*as.numeric(as.character(tidydata$exp_value)) + as.numeric(as.character(tidydata$CROPDMG))*as.numeric(as.character(tidydata$exp_crop)))
 
-# fun <- function(x){
-#   print(x)
-#   if(x='M'){temp<-as.numeric(as.character(tidydata$PROPDMG))*1e6
-# }}
-# expRows <- length(tidydata$PROPDMGEXP)
-# tidydata$PROPDMGEXP_convert <- sapply(1:expRows, FUN=fun(as.character(tidydata$PROPDMGEXP)))
-# 
-# # tidydata$PROPDMGEXP_convert <- sapply(tidydata$PROPDMGEXP, function(x)(as.numeric(temp<-gsub("B", "", x))*1000000000))
-# # tidydata$PROPDMGEXP_convert <- sapply(tidydata$PROPDMGEXP, function(x)(as.numeric(temp<-gsub("M", "", x))*1000000))
-# # tidydata$PROPDMGEXP_convert <- sapply(tidydata$PROPDMGEXP, function(x)(as.numeric(temp<-gsub("K", "", x))*1000))
+summaryEconData <- aggregate(cbind(tidydata$EVTYPE, tidydata$death_injury, tidydata$economic), by=list(tidydata$EVTYPE), FUN=sum, na.rm=TRUE, na.action=NULL)
+
+#summarydata <- tidydata[order(-tidydata$economic), ]
+
+colnames(summaryEconData) <- c("Event", "Death_Injury", "Economic")
+summaryEconData <- summaryEconData[order(-summaryEconData$Death_Injury), ]
+head(summaryEconData)
+
+###plot graph
+ggplot(data=head(summaryEconData,10), aes(x=Event, y=Death_Injury)) + 
+     stat_summary(fun.y=sum, geom="bar", color = "black", size=1) +
+     theme( axis.ticks.x=element_blank())
+
+summaryEconData <- summaryEconData[order(-summaryEconData$Economic), ]
+head(summaryEconData)
+
+ggplot(data=head(summaryEconData,10), aes(x=Event, y=Economic)) + 
+     stat_summary(fun.y=sum, geom="bar", color = "black", size=1) +
+     theme(axis.ticks.x=element_blank())
